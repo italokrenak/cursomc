ef53661d234a918146fc3eac5b0661aaa7b7f2fd
italo_leitao <italokrenak@gmail.com>
2018-01-28 16:55:27 -0300

    Criando repository e service para categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -3,25 +3,38 @@ package com.nelioalves.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
+
+	@Autowired
+	private CategoriaService categoriaService;
+
+	/*@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar() {
 		Categoria categoria1 = new Categoria(1, "Informátiva");
 		Categoria categoria2 = new Categoria(2, "Escritório");
-		
+
 		List<Categoria> lista = new ArrayList<Categoria>();
 		lista.add(categoria1);
 		lista.add(categoria2);
-		
+
 		return lista;
+	}*/
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = this.categoriaService.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.nelioalves.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+
+	public Categoria buscar(Integer id) {
+		Categoria categoria = this.repository.findOne(id);
+		return categoria;
+	}
+}
