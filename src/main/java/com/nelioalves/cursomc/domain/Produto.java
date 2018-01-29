ae133b0ae2303ac5b612e1eb62d97b3fa60819e7
italokrenak <italokrenak@gmail.com>
2018-01-29 14:29:24 -0300

    Ajuste nos endpoint /categorias/{id}

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -22,6 +24,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 
+	@JsonBackReference // Evita problema da referecnia cíclica (Do outro lado da associação já buscado os obejtos, logo não preencha esses dados).
 	@ManyToMany
 	@JoinTable(name = "produto_categoria", 
 		joinColumns = @JoinColumn(name = "produto_id"), 
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -5,15 +5,22 @@ import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
 
 	public Categoria buscar(Integer id) {
-		Categoria categoria = this.repository.findOne(id);
-		return categoria;
+		Categoria obj = this.repository.findOne(id);
+
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
+
+		return obj;
 	}
 }
