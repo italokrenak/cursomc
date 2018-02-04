0098b325f8f6fdd34de8ab645f0a18f768e5a06e
italokrenak <italokrenak@gmail.com>
2018-02-04 18:42:37 -0300

    Paginação com parâmetros opcionias na requisicão

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -70,7 +70,12 @@ public class CursomcApplication implements CommandLineRunner {
 		// Many To Many
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônica");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+		
 		Produto p1 = new Produto(null, "Computardor", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.0);
@@ -82,7 +87,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		this.categoriaRepository.save(Arrays.asList(cat1, cat2));
+		this.categoriaRepository.save(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		this.produtoRepository.save(Arrays.asList(p1, p2, p3));
 
 		// Many To One and One To Many
